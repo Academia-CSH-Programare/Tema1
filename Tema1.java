072b3796dc78d2012394135035a522843dac934e
Dany <perhaitadaniel14@gmail.com>
2021-07-23 14:39:29 +0300

    Tema1

---
+++ Tema1.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+	int a = 1;
+	float b = 2.3f;
+	double c = 4.56;
+	String d = "Ok";
+
+    public static void main(String[] args) {
+	    /*int a = 1;
+	    float b = 2.3f;
+	    double c = 4.56;
+	    String d = "Ok";*/
+
+		Main o = new Main();
+
+		System.out.println(o.a);
+		System.out.println(o.b);
+		System.out.println(o.c);
+		System.out.println(o.d);
+    }
+}
